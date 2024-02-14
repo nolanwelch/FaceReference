23425b5b6419cad9aff9dae932ccedcaad6625e8
Nolan Welch <nolanwelch@outlook.com>
2024-02-14 23:34:28 +0100

    Create basic install script

--- install.sh
+++ install.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-./cfg.sh
+echo Beginning install ...
+sleep 1
 
+source cfg.sh
+echo Loaded config.
+
+echo Creating virtual environment ...
 python3 -m venv env
 source env/bin/activate
+
+echo Upgrading pip ...
+pip install --upgrade pip
+
+echo Installing from requirements.txt ...
 pip install -r requirements.txt
 
-touch FLASK_DATABASE
-sqlite3 FLASK_DATABASE < facereference/schema.sql
+echo Initializing database ...
+touch $FLASK_DATABASE
+sqlite3 $FLASK_DATABASE < facereference/schema.sql
+
+echo Done!
+rm -- "$0"
+
+exit
