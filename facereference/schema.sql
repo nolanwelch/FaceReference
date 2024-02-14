008874e2dd5a185f2d9c7b3aee21b055016d4228
Nolan Welch <nolanwelch@outlook.com>
2024-02-14 23:52:42 +0100

    Expand schema

--- facereference/schema.sql
+++ facereference/schema.sql
@@ -1,8 +1,32 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS images;
+DROP TABLE IF EXISTS imagetags;
+DROP TABLE IF EXISTS tags;
 
 CREATE TABLE users (
     username TEXT PRIMARY KEY,
     dateCreated TEXT NOT NULL,
     firstName TEXT NOT NULL,
     lastName TEXT NOT NULL
+);
+
+CREATE TABLE images (
+    id INT PRIMARY KEY,
+    uri TEXT NOT NULL,
+    heightPx INT NOT NULL,
+    widthPx INT NOT NULL,
+    altText TEXT
+);
+
+CREATE TABLE imagetags (
+    imageId INT,
+    tagId INT,
+    FOREIGN KEY(imageId) REFERENCES images(id),
+    FOREIGN KEY(tagId) REFERENCES tags(id)
+);
+
+CREATE TABLE tags (
+    id INT,
+    tag TEXT,
+    PRIMARY KEY (id, tag)
 );
