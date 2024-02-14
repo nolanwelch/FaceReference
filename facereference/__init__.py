38babd721dfbc7ec878847a84ab9c01e1d454966
Nolan Welch <nolanwelch@outlook.com>
2024-02-14 23:12:59 +0100

    Expand views and init

--- facereference/__init__.py
+++ facereference/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
+# tutorial: https://python-adv-web-apps.readthedocs.io/en/latest/flask.html
+
 app = Flask(__name__)
+app.config.from_prefixed_env()
 
 import facereference.views
--- facereference/views.py
+++ facereference/views.py
@@ -1,6 +1,18 @@
 from facereference import app
+from facereference.util import queryUser
 
 
 @app.route("/")
 def index():
-    return "# FaceReference v0.1"
+    top = "<h1>FaceReference v0.1</h1>"
+    redirect = "<p>Click <a href='user/nolan'>here</a> to see the user page!"
+    return top + redirect
+
+
+@app.route("/user/<name>")
+def user(name):
+    data = queryUser(name)
+    if not data:
+        return f"Sorry, but it doesn't look like user \"{name}\" exists."
+
+    return f"<h2>Hello, {name}!"
